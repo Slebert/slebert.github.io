4833889aef1abdced7769bc61b9ce816e23015d8
_z <jhin@tuta.io>
2023-03-05 04:43:02 -0500

    Fix non-uniform random number distribution

--- js/bgColor.js
+++ js/bgColor.js
@@ -1,4 +1,4 @@
 function bgRandColor() {
-    var rand24 = Math.round(0xffffff * Math.random());
-    document.querySelector(':root').style.setProperty('--color1',(rand24 >> 16)+','+(rand24 >> 8 & 255)+','+(rand24 & 255));
+    var rand24 = Math.floor(Math.random() * 0x1000000);
+    document.querySelector(':root').style.setProperty('--color1',(rand24 >> 0x10)+','+(rand24 >> 0x08 & 0xFF)+','+(rand24 & 0xFF));
 };
