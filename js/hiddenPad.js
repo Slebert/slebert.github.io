82e8b85140992fb998a7de68779b01df446aeece
Slebert <lebert.spencer@gmail.com>
2023-03-08 10:04:19 -0500

    Added Hidden Keypad
    added hidden keypad for random stuff, still working on the look of keypad iIknow it looks terrible right now lmao

---
+++ js/hiddenPad.js
@@ -0,0 +1,29 @@
+function setScreen(x) {
+	document.getElementById("screen").innerHTML = x; 
+}
+function keyPressed(key){
+	const current = String(document.getElementById("screen").innerHTML);
+	if (current.length < 10){
+		document.getElementById("screen").innerHTML += key;
+	}
+
+}
+function submit(){
+	const submission = document.getElementById("screen").innerHTML;
+	/* cheater :p */
+	switch(submission){
+		case '69':
+			
+		case '420':
+			
+		case '4848':
+			bgRandColor();
+	}
+}
+function minMax() {
+	if (document.getElementById('popOut').style.display == 'inline'){
+		document.getElementById('popOut').style.display = 'none';
+	}else{
+		document.getElementById('popOut').style.display = 'inline';
+	}
+}
